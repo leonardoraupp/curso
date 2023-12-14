bd48a4e2ee464b696beefaf479e5c0c3a4c1a54c
ctllraupp <ctl_lraupp@uolinc.com>
2023-12-14 10:27:35 -0300

    primeiras aulas de fundamentos

---
+++ fundamentos/inicio/desafio_area.js
@@ -0,0 +1,7 @@
+// Calcular a área de uma circunferência.
+
+const PI = 3.141592;
+const raio = 10;
+const areaCirc = PI * raio * raio;
+
+console.log("O valor da área é: " + areaCirc + "m²");
---
+++ fundamentos/inicio/desafio_troca.js
@@ -0,0 +1,17 @@
+// Desafio troca de valores das variáveis.
+let a = 7;
+let b = 94;
+
+// a = 94;
+// b = 7;
+
+// Usando uma variável temporária
+// let temp = a;
+// a = b;
+// b = temp;
+
+// Usando array
+[a, b] = [b, a];
+
+console.log(a);
+console.log(b);
